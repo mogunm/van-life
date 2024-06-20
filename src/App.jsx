aefe20772c9c6800702beff51abdb15efb5c264a
mogunm <michaelogunmosin40@gmail.com>
2024-06-20 01:22:31 -0500

    updated file paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
-import Home from "./Home"
-import About from "./About"
+import Home from "./pages/Home"
+import About from "./pages/About"
 
 export default function App() {
   return (
